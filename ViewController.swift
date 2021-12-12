b84c13678d3423905123271eb44817c10250fafc
ManhLV <wc.ralph207@gmail.com>
2021-12-12 22:01:36 +0700

    commit 2

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Major_flag
+//
+//  Created by Apple on 12/12/21.
+//  Copyright © 2021 Unstoppable. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
